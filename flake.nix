26ef49f0a38958171700d97a2b61500ee7bce4ce
Jacob Colvin <jacobcolvin1@gmail.com>
2025-06-28 21:23:58 -0400

    feat: remove legacyPackages

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   outputs = { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
+      allPackages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
       });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) allPackages.${system});
     };
 }
