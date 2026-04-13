a953a23b7ae39a3c7735a1f8d8843aeff57e8ab8
botty-mcbottington <168950816+botty-mcbottington[bot]@users.noreply.github.com>
2026-04-13 00:29:51 +0000

    chore: pkg update for terrarium v0.6.0

--- pkgs/terrarium/default.nix
+++ pkgs/terrarium/default.nix
@@ -11,18 +11,18 @@
 let
   inherit (stdenvNoCC.hostPlatform) system;
   shaMap = {
-    x86_64-linux = "sha256-o7PmbrrW3yLVL6gv+TBDd2lGqkBMhtwkgzsBBtVS63w=";
-    aarch64-linux = "sha256-wSQ4YIlmOicrOWz9civMA1XQjhdzoqq0JGNhD0gNs+U=";
+    x86_64-linux = "sha256-zEtxWl5zws/opWMRAJYtOB13Zf/xjUa+U11nIje7evo=";
+    aarch64-linux = "sha256-YMBbOLfg1DR8ZDYfO8jjNVsHQ3Umlhr3r9MNiYisLZs=";
   };
 
   urlMap = {
-    x86_64-linux = "https://github.com/macropower/terrarium/releases/download/v0.5.0/terrarium_Linux_x86_64.tar.gz";
-    aarch64-linux = "https://github.com/macropower/terrarium/releases/download/v0.5.0/terrarium_Linux_arm64.tar.gz";
+    x86_64-linux = "https://github.com/macropower/terrarium/releases/download/v0.6.0/terrarium_Linux_x86_64.tar.gz";
+    aarch64-linux = "https://github.com/macropower/terrarium/releases/download/v0.6.0/terrarium_Linux_arm64.tar.gz";
   };
 in
 stdenvNoCC.mkDerivation {
   pname = "terrarium";
-  version = "0.5.0";
+  version = "0.6.0";
   src = fetchurl {
     url = urlMap.${system};
     sha256 = shaMap.${system};
